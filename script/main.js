bf5ca5bcb32798986dc89ffed58fd726775336e8
tgt26 <38292380+tgt26@users.noreply.github.com>
2018-09-03 21:19:32 -0400

    added more questions to the quiz

--- script/main.js
+++ script/main.js
@@ -21,6 +21,55 @@ const questions = [{
     option3: 'Papa John\'s',
     option4: 'Arby\'s',
     answer: '4'
+}, {
+    question: 'Grand Central Terminal, Park Avenue, New York is the world\'s',
+    option1: 'largest railway station',
+    option2: 'highest railway station',
+    option3: 'longest railway station',
+    option4: 'None of the above',
+    answer: '1'
+}, {
+    question: 'Entomology is the science that studies',
+    option1: 'Behavior of human beings',
+    option2: 'Insects',
+    option3: 'The origin and history of technical and scientific terms',
+    option4: 'The formation of rocks',
+    answer: '2'
+}, {
+    question: 'Eritrea, which became the 182nd member of the UN in 1993, is in the continent of: ',
+    option1: 'Asia',
+    option2: 'Africa',
+    option3: 'Europe',
+    option4: 'Australia',
+    answer: '2'
+}, {
+    question: 'Garampani sanctuary is located at',
+    option1: 'Junagarh, Gujarat',
+    option2: 'Diphu, Assam',
+    option3: 'Kohima, Nagaland',
+    option4: 'Gangtok, Sikkim',
+    answer: '2'
+}, {
+    question: 'For which of the following disciplines is Nobel Prize awarded?',
+    option1: 'Physics and Chemistry',
+    option2: 'Physiology or Medicine',
+    option3: 'Literature, Peace and Economics',
+    option4: 'All of the above',
+    answer: '4'
+}, {
+    question: 'Hitler party which came into power in 1933 is known as',
+    option1: 'Labour Party',
+    option2: 'Nazi Party',
+    option3: 'Ku-Klux-Klan',
+    option4: 'Democratic Party',
+    answer: '2'
+}, {
+    question: 'FFC stands for',
+    option1: 'Foreign Finance Corporation',
+    option2: 'Film Finance Corporation',
+    option3: 'Federation of Football Council',
+    option4: 'None of the above',
+    answer: '2'
 }];
 // Declarations
 let currentQuestion = 0;
@@ -73,7 +122,7 @@ nextBtn.addEventListener('click', function () {
     if (currentQuestion == totalQuestion) {
         quizContainer.style.display = 'none';
         resultContainer.style.display = 'inline-grid';
-        resultText.innerHTML = 'Your Score: ' + score + '!';
+        resultText.innerHTML = 'Your Score: ' + score + '/' + totalQuestion * 10;
         return;
     }
     loadQuestion(currentQuestion);
